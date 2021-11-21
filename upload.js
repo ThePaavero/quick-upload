587a1af32ce07fa42a46b79c12a17c0182c87ba9
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-11-21 19:46:40 +0200

    Optional SSH key stuff.

--- upload.js
+++ upload.js
@@ -5,10 +5,22 @@ const { argv } = require('process')
 const config = require('./.env.json')
 
 const workingDir = process.cwd()
-const filename = argv[2].trim()
-const sourcePath = `${workingDir}/${filename}`
-const command = `scp -i ${config.sshKeyPath} ${sourcePath} ${config.sshUsername}@${config.remoteDomain}:${config.uploadDirPath}`
+const filename = argv[2] ? argv[2].trim() : null
 
+if (!filename) {
+  console.log('You need to give me a file name.')
+  process.exit(-1)
+}
+
+const getPossibleKeyFlag = () => {
+  if (!config.sshKeyPath) {
+    return ''
+  }
+  return `-i ${config.sshKeyPath}`
+}
+
+const sourcePath = `${workingDir}/${filename}`
+const command = `scp ${getPossibleKeyFlag()} ${sourcePath} ${config.sshUsername}@${config.remoteDomain}:${config.uploadDirPath}`
 const response = execSync(command).toString()
 
 console.log(response)
