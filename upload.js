b028ea43e22642e03ef422fdc4d4de340c3f96ce
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-11-21 19:40:58 +0200

    SSH key issues...

--- upload.js
+++ upload.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
+const { execSync } = require('child_process')
 const fs = require('fs')
+const { argv } = require('process')
 const config = require('./.env.json')
 
-console.log(config)
+const workingDir = process.cwd()
+const filename = argv[2].trim()
+const sourcePath = `${workingDir}/${filename}`
+const command = `scp -i ${config.sshKeyPath} ${sourcePath} ${config.sshUsername}@${config.remoteDomain}:${config.uploadDirPath}`
+
+const response = execSync(command).toString()
+
+console.log(response)
